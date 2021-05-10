89af689b0b78437266c24957d2f2cb925ff87942
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-10 17:27:37 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+
+		 int a = 7, b = 11;
+		    System.out.println("a is " + a + " and b is " + b);
+		    System.out.println(a == b);
+		    System.out.println(a != b);
+		    System.out.println(a > b); 
+		    System.out.println(a < b);  
+		    System.out.println(a >= b);
+		    System.out.println(a <= b);  
+
+		
+	}
+
+}
