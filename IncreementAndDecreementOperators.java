597b74bd7117af1ef14d1c73937ceca81948a24e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-10 17:26:48 +0530

    Add files via upload

---
+++ IncreementAndDecreementOperators.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class IncreementAndDecreementOperators {
+
+	public static void main(String[] args) {
+
+		
+	        int var1 = 5, var2 = 5;
+	        System.out.println(var1++);
+	        System.out.println(++var2);
+	    }
+		
+	}
+
