3de16d99c529158f6c0510469f717c60158130ee
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-10 17:26:59 +0530

    Add files via upload

---
+++ InstanceOfOperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class InstanceOfOperator {
+
+	public static void main(String[] args) {
+
+
+	    String str = "Programiz";
+	    boolean result;
+	    result = str instanceof String;
+	    System.out.println("Is str an object of String? " + result);
+
+		
+	}
+
+}
