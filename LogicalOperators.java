71b4651d8e6f2972c8f66a8d6629c37ebae032b8
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-10 17:27:10 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		
+			System.out.println((5 > 3) && (8 > 5));  
+		    System.out.println((5 > 3) && (8 < 5));
+		    System.out.println((5 < 3) || (8 > 5));  
+		    System.out.println((5 > 3) || (8 < 5));  
+		    System.out.println((5 < 3) || (8 < 5));  
+		    System.out.println(!(5 == 3));  
+		    System.out.println(!(5 > 3));
+
+		
+	}
+
+}
