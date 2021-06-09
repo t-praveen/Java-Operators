3c32ded59b784af3cb2e0f28c4ab8559cb7dcd5e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-09 14:43:48 +0530

    Add files via upload

---
+++ Ternary2.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Ternary2 {
+
+	public static void main(String[] args) {
+
+		boolean isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+        {
+            System.out.println("wasCar is true");
+        }
+    }
+}
+
+
