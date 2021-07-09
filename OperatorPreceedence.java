8efadd415cfaf4b0cb901bc83019301551b0c7e5
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-07-09 23:19:22 +0530

    Update OperatorPreceedence.java

--- OperatorPreceedence.java
+++ OperatorPreceedence.java
@@ -1,16 +1,24 @@
-//Operator Preceedence//
+
+//Operator Program//
 
 package operators;
 
-public class OperatorPreceedence {
+public class OperatorChallenge {
 
 	public static void main(String[] args) {
-
-		int a = 10, b = 5, c = 1, result;
-    	result = a-++c-++b;
-    	
-    	System.out.println(result);
 		
+		double first_var = 20.00d ;
+		double second_var = 80.00d ;
+		double result1 = (first_var + second_var) * 100.00d ;
+		System.out.println("The first operation result is : "+result1);
+		double result2 = result1 % 40.00d ;
+		System.out.println("The remainder is : "+result2);
+		boolean result3 = (result2 == 0) ? true : false ;
+		System.out.println("The result is : "+result3);
+		if(result3 == false)
+		{
+			System.out.println("Got Some Remainder .");
+		}
 	}
 
 }
