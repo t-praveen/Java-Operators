df6f0027a8073116dd16de0efb8e9a4b90a939c1
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-08-31 09:55:48 +0530

    Update AllOperators.java

--- AllOperators.java
+++ AllOperators.java
@@ -15,7 +15,7 @@ public class AllOperators {
 
         result = result * 10;   // 2 * 10 = 20 
         System.out.println("2 * 10 = " + result);
-
+ 
         result = result / 5;    // 20 / 5 = 4
         System.out.println("20 / 5 = " + result); 
 
