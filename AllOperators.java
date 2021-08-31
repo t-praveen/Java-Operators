4588f34846c32ac54abc39439c835bbee79eb248
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-08-31 09:54:58 +0530

    Update AllOperators.java

--- AllOperators.java
+++ AllOperators.java
@@ -5,7 +5,7 @@ public class AllOperators {
  
     public static void main(String[] args) {
 
-        int result = 1 + 2; // 1 + 2 = 3  
+        int result = 1 + 2; // 1 + 2 = 3   
         System.out.println("1 + 2 = " + result);
         int previousResult = result;
         System.out.println("previousResult =  " + previousResult);
